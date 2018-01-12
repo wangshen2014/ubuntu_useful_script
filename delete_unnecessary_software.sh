31be7490d7559a2da2b8b856a11df60c942fb91e
wangshen2014 <bestwisheswangshen@163.com>
2018-01-12 17:08:27 -0600

    delete_unnecessary_software

---
+++ delete_unnecessary_software.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get -y –auto-remove purge unity-2d*
+sudo apt-get -y purge empathy
+sudo apt-get -y purge nautilus-sendto-empathy
+sudo apt-get -y purge empathy-common #即时通讯
+sudo apt-get -y purge thunderbird* #邮件
+sudo apt-get -y purge gwibber* #微博
+sudo apt-get autoremove gcalctool #计算器
+sudo apt-get -y purge ubuntuone* #Ubuntu One
+sudo apt-get -y purge deja-dup #备份
+sudo apt-get -y purge bluez* #卸载蓝牙
+sudo apt-get -y purge simple-scan #扫描
+sudo apt-get -y purge hplip* #打印
+sudo apt-get -y purge printer-driver* #打印驱动
+sudo apt-get -y purge rhythmbox* #音乐播放
+sudo apt-get -y purge gedit* #文本编辑
+sudo apt-get -y purge libreoffice* #办公套件
+sudo apt-get -y purge gnome-orca #屏幕阅读
+sudo apt-get -y purge onboard #屏幕键盘
+sudo apt-get -y purge mahjongg #对对碰
+sudo apt-get -y purge aisleriot #纸牌王
+sudo apt-get -y purge gnome-sudoku #数独
+sudo apt-get -y purge gnomine #扫雷
+sudo apt-get -y purge wodim #命令刻碟
