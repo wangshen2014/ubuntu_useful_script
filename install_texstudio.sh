8c746ab6bfe4d3f47fa7ddd6606cf95356f377ee
sichu366 <sichu@ucsc.edu>
2019-09-20 13:39:52 -0500

    update the method of install_texstudio

--- install_texstudio.sh
+++ install_texstudio.sh
@@ -1,3 +1,4 @@
+sudo apt-get remove texstudio-d*
 sudo apt-get update
 sudo apt-get -y install texlive-fonts-recommended
 sudo apt-get -y install texlive
@@ -5,3 +6,5 @@ sudo apt-get -y install texlive-xetex
 sudo apt-get -y install texlive-science
 sudo apt-get -y install texlive-publishers
 sudo apt-get -y install texstudio
+sudo apt-get install texlive-bibtex-extra biber
+sudo apt-get install texlive-fonts-extra
